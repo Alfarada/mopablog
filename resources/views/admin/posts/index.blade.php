2eb4f117c8962ec210d6c5b284339be5c9fb53b5
Alfredo <alfaradayepez@gmail.com>
2020-10-01 20:31:40 -0500

    Creando componente para las tablas

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -2,56 +2,49 @@
 
 @section('content')
 
-    @component('shared._card')
-    
+@component('shared._card') {{-- OPEN CARD COMPONENT --}}
+
+    @slot('header')
+        Lista de posts
+        <a class="btn btn-sm btn-primary float-right"
+            href=" {{ route('posts.create') }}"> Crear post</a>
+    @endslot
+
+    @slot('content') {{-- OPEN CONTENT SLOT --}}
+
+    @component('shared._table') {{-- OPEN TABLE COMPONENT --}}
+        
         @slot('header')
-            Lista de posts
-            <a class="btn btn-sm btn-primary float-right" href=" {{ route('posts.create') }}"> Crear post</a>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Detalles</th>
+            <th colspan="3">&nbsp;</th>
         @endslot
 
-        @slot('content')
-        <table class="table table-light">
-            <thead class="thead-light">
-                <tr>
-                    <th width="10px">ID</th>
-                    <th>Nombre</th>
-                    <th>Detalles</th>
-                    <th colspan="3">&nbsp;</th>
-                </tr>
-            </thead>
-            <tbody>
+        @slot('body') {{-- OPEN BODY SLOT--}}
+            @foreach ($posts as $post)
+            <tr>
+                <td>{{ $post->id }}</td>
+                <td>{{ $post->title }}</td>
 
-                @foreach ($posts as $post)
-                <tr>
-                    <td>{{ $post->id }}</td>
-                    <td>{{ $post->title }}</td>
-                    <td with="10px">
-                        {{-- Show post Link --}}
-                        <a id="ver" href=" {{ route('posts.show', $post->url_attr) }}" class="btn btn-sm btn-light">
-                            ver
-                        </a>
-                    </td>
-                    <td with="10px">
-                        {{-- Update post link  --}}
-                        <a href="{{ route('posts.edit', $post->url_attr) }}" class="btn btn-sm btn-light">
-                            editar
-                        </a>
-                    </td>
-                    <td with="10px">
-                        {!! Form::open([
-                        'route' => ['posts.destroy', $post],
-                        'method' => 'DELETE']) !!}
+                <td><a id="ver" href="{{ route('posts.show', $post->url_attr) }}"
+                        class="btn btn-sm btn-light"> ver </a></td>
 
+                <td><a href="{{ route('posts.edit', $post->url_attr) }}"
+                        class="btn btn-sm btn-light"> editar </a></td>
+
+                <td>
+                    {!! Form::open(['route' => ['posts.destroy', $post],'method' => 'DELETE']) !!}
                         <button class="destroy btn btn-sm btn-danger"> Eliminar </button>
+                    {!! Form::close()!!}
+                </td>
+            </tr>
+            @endforeach   
+        @endslot {{-- END BODY SLOT--}}
+    @endcomponent {{-- END TABLE COMPONENT --}}
 
-                        {!! Form::close()!!}
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-        {{ $posts->render() }}
-        @endslot
-@endcomponent
+    {{ $posts->render() }}  {{-- POST PAGINATION --}}
 
+    @endslot    {{-- END CONTENT SLOT --}}   
+@endcomponent {{-- END CARD COMPONENT --}}
 @endsection
---
+++ resources/views/shared/_table.blade.php
@@ -0,0 +1,12 @@
+<table class="table table-light">
+    <thead class="thead-light">
+        <tr>
+            {{ $header }}
+        </tr>
+    </thead>
+    <tbody>
+        {{ $body }}
+    </tbody>
+</table>
+
+
